a0dd11114b57326566eed4717453ba337ec7837f
setare askari <setareaskari79@gmail.com>
2021-10-16 17:24:59 +0200

    ADD test class for Office

---
+++ OfficeQueueManagement/src/test/java/office/OfficeTest.java
@@ -0,0 +1,18 @@
+package office;
+
+import static org.junit.Assert.*;
+
+public class OfficeTest {
+
+    @org.junit.Before
+    public void setUp() throws Exception {
+    }
+
+    @org.junit.After
+    public void tearDown() throws Exception {
+    }
+
+    @org.junit.Test
+    public void notifyThatCounterIsFree() {
+    }
+}
