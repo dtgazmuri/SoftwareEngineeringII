0fe49447aa88c742b706770a8eb83157bc6c6fed
stestrippoli <80058558+stestrippoli@users.noreply.github.com>
2021-10-18 00:34:28 +0200

    Finished DBManager.java
    Check if all the functions are needed

--- OfficeQueueManagement/src/main/java/db/DBManager.java
+++ OfficeQueueManagement/src/main/java/db/DBManager.java
@@ -6,54 +6,76 @@ import java.sql.SQLException;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.text.SimpleDateFormat;
-import java.util.HashMap;
+import java.util.ArrayList;
 import java.util.Date;
 import office.Ticket;
 import office.ServiceType;
+
 public class DBManager {
-	
-	public Connection connection;
-	
-	//Nothing inside for now...
-	
-	public DBManager() {
-        try
-        {
-		  this.connection = null;
-		  String url = "jdbc:sqlite:se2.db";
-          this.connection = DriverManager.getConnection(url);
-		  System.out.println("Connection estabilished!");
-		} catch (SQLException e) {
-        	throw new Error("Error, connection failed.", e);
-    	}
-	}
 
-	
-
-	/*
-	* The function returns the HashMap between Type of Services and its IDs.
+	/**Create a connection with the database.
 	 * 
-	 * @return HashMap <Integer, String> of all the Type of Services
-	public static HashMap<Integer, String> getTypeOfServiceHashMap(){
-		HashMap<Integer,String> typeofservice = new HashMap<Integer, String>()
+	 * @return The connection
+	 * @throws SQLException
+	 */
+    public static Connection getConnection() throws SQLException {
+		Connection conn=null;
+		try {
+            Class.forName("org.sqlite.JDBC");
+			conn= DriverManager.getConnection("jdbc:sqlite:se2.db");
+			System.out.println("Connection with the db estabilished");	
+        }
+        catch (ClassNotFoundException e) {
+			System.out.println(e);
+        }
+		return conn;
+    }
+
+
+	//Useless?
+
+	
+	/** The function returns the list of Type of Services with all its informations.
+	 * @return ArrayList<ServiceType> 
+	*/
+
+	public ArrayList<ServiceType> getServiceType(){
+		ArrayList<ServiceType> STList  = new ArrayList<ServiceType>();
 		Statement stmt = null;
-		String query = "SELECT * FROM TypeOfService";
+		Connection connection = null;
+		String query = "SELECT * FROM servicetype";
+		try{
+			connection = getConnection();
+		}
+		catch(SQLException e){ 
+			System.out.println(e);
+		}
+		
 		try{	
+			stmt = connection.createStatement();
 			ResultSet rs = stmt.executeQuery(query);
 			while (rs.next()){
-				String name = rs.getString("NAME")
+				String name = rs.getString("NAME");
 				int id = rs.getInt("ID");
-				typeofservice.put(id,name);
+				double time = rs.getDouble("TIME");
+				STList.add(new ServiceType(id, name, time));
 			}
-			return typeofservice;
+			return STList;
 		}
 		catch(SQLException e){
 			throw new Error("Problem on querying the database", e);
 		} finally {
-			if (stmt != null) { stmt.close(); }
+			if (stmt != null) {
+				try{
+					stmt.close();
+				}
+				catch(SQLException e){
+					System.out.println(e);
+				} 
+			}
 		}
 	}
-	*/
+	
 
 	private static String getDate(){
 		Date date = new Date();
@@ -64,28 +86,37 @@ public class DBManager {
 
 
 	/** The function add to the database the last ticked processed 
-	 * @return 			....
-	 * */
+	 * @throws SQLException
+	 * @param t the new ticket
+	 * @param counter the counter who processed the ticket	 
+	 * **/
 	
-	 public void addTicket(Ticket t, int counter){
-		int id = t.getId();
+	 public void addTicket(Ticket t, int counter) throws SQLException{
 		ServiceType st = t.getServiceType();
 		String date = getDate();
-		Statement stmt = null;
-		String sql = "INSERT INTO Tickets VALUES(?,?,?,?)";
+		Connection connection = getConnection();
+		PreparedStatement prepState = null;
+		String sql = "INSERT INTO tickets VALUES(NULL,?,?,?)";
 		try{	
-			PreparedStatement prepState =
-				this.connection.prepareStatement(sql);
-			prepState.setInt(1, id);
-			prepState.setInt(2, st.getId());
-			prepState.setInt(3, counter);
-			prepState.setString(4, date);
-			int rowsAffected = prepState.executeUpdate();
+			prepState =	connection.prepareStatement(sql);
+			prepState.setInt(1, st.getId());
+			prepState.setInt(2, counter);
+			prepState.setString(3, date);
+			prepState.executeUpdate();
 		}
 		catch(SQLException e){
 			throw new Error("Problem on querying the database", e);
 		}
+		finally {
+			if (prepState != null) {
+				try{
+					prepState.close();
+				}
+				catch(SQLException e){
+					System.out.println(e);
+				} 
+			}
+		}
 	}
 
-	 
 }
